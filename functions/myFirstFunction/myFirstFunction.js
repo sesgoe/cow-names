dcc3130dba7bcb8919d5a66734bdbc5259901ea2
sesgoe <jsesgoe@gmail.com>
2020-05-02 19:04:42 -0400

    trying lambda chromium build on actual AWS

--- functions/myFirstFunction/myFirstFunction.js
+++ functions/myFirstFunction/myFirstFunction.js
@@ -1,6 +1,23 @@
-exports.handler = function(event, context, callback) {
+const chromium = require('chrome-aws-lambda')
+
+exports.handler = async function(event, context, callback) {
+    const browser = await chromium.puppeteer.launch({
+        headless: true
+    })
+
+    const page = await browser.newPage()
+    await page.goto(
+        'https://absbullsearch.absglobal.com/Search/Bull?animal=Ses&proof=USA&country=USA'
+    )
+
+    const content = await page.content()
+
+    if (browser != null) {
+        await browser.close()
+    }
+
     callback(null, {
         statusCode: 200,
-        body: "Hello, world! I'm a netlify function!"
+        body: content
     })
 }
